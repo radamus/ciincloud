8e6cc6e95134a3f7a313457f6d1defdc1893ff1b
Radek Adamus <adamus.r@gmail.com>
2014-04-26 17:40:39 +0200

    first version of discount

--- discount.js
+++ discount.js
@@ -1,1 +1,16 @@
-module.exports.discount = function(){}
+module.exports.discount = function(amount, rates){
+	var discount = 0;
+	var tresholds = Object.keys(rates);
+
+	for(var i = 0; i < tresholds.length; i++){
+		var key = tresholds[i];
+		var treshold = parseInt(key);
+		var discountPercentage = rates[key];
+		if(amount > treshold){
+			discount = amount * discountPercentage * 0.01;
+		}
+	}
+
+	return discount;
+				
+}
--- test/test.js
+++ test/test.js
@@ -2,9 +2,22 @@ var assert = require("assert");
 var discountCalculator = require("../discount");
 
 
+
+
 describe("discount Calculator", function(){
 	it("should have discount function", function(){
 		assert.equal(discountCalculator.discount instanceof Function, true, "discountCalculator.discount instanceof Function"); 		
+	});
+});
+
+describe("discount function", function(){
+	it("discount should be 0% below single treshold ", function(){
+		var rates = {"1000":5}
+		var amount = 999;
+
+		var discount = discountCalculator.discount(amount, rates);
+		assert.equal(discount, 0, discount + "");
+
 	});
 });
 
