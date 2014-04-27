59d1eb96554831ca4c3be28fbbd36377d8c443c9
Radek Adamus <adamus.r@gmail.com>
2014-04-27 07:07:12 +0200

    tests refactorization

--- test/test.js
+++ test/test.js
@@ -11,30 +11,31 @@ describe("discount Calculator", function(){
 });
 
 describe("discount function", function(){
-	var rates;
+	var singleTresholdRates;
 	
 	beforeEach(function(){
-		rates = {"1000":5}
-		
-
+		singleTresholdRates = {"1000":5}		
 		});
 
-	it("discount should return 0 by default  ", function(){		
+	it("discount should return 0 with no args  ", function(){		
 		var discount = discountCalculator.discount();
 		assert.equal(discount, 0, discount + "");
 
 	});
 
-	it("discount should be 0  ", function(){
-		var amount = 999;
-		var discount = discountCalculator.discount(amount, rates);
-		assert.equal(discount, 0, discount + "");
+	describe("when amount is lower than a single treshold ", function(){
+		it("discount should be 0  ", function(){
+			var amount = 999;
+			var discount = discountCalculator.discount(amount, singleTresholdRates);
+			assert.equal(discount, 0, discount + "");
 
+		});
 	});
+	
 
 	it("discount should be 50,05  ", function(){
 		var amount = 1001;
-		var discount = discountCalculator.discount(amount, rates);
+		var discount = discountCalculator.discount(amount, singleTresholdRates);
 		assert.equal(discount, 50.05, discount + "");
 
 	});
